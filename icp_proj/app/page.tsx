76e50ac2ff354e05df311f734458a8db4fb49aac
kew986 <katecoleen.oliverio-21@cpu.edu.ph>
2024-07-18 15:50:44 +0800

    initial

--- icp_proj/app/page.tsx
+++ icp_proj/app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex nw-screen text-5xl font-bold min-h-screen flex-col items-center justify-between p-24">
-      Hello my name is Kate Coleen
-      
+      Hello my name is Kate Coleen, I am a beginner.
     </main>
   );
 }
