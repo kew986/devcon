75564106ce063e8c8a48c3e5c72c6b85fad52ae4
kew986 <katecoleen.oliverio-21@cpu.edu.ph>
2024-07-18 15:11:00 +0800

    initial

---
+++ icp_proj/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex nw-screen text-5xl font-bold min-h-screen flex-col items-center justify-between p-24">
+      Hello my name is Kate Coleen
+      
+    </main>
+  );
+}
